fc92d2818f31b777f9b94c9e4f23cc87835899c1
MJPeternell <mjpeternell@gmail.com>
2017-09-20 11:54:13 -0700

    add button to recent work section

--- wp-content/themes/megatherium/underscore-template-web-portfolio_recent.php
+++ wp-content/themes/megatherium/underscore-template-web-portfolio_recent.php
@@ -17,27 +17,15 @@
                                 <div class="panel-heading">
                                     <h1 class="panel-title"><a href="<%= item.link %>" title="<%= item.title.rendered %>"><%= item.title.rendered %></a></h1>
                                 </div>
-<!--                                <p class="lead"><%= item.kia_subtitle %></p>                       
-                                <% if ( item.cpt_tag ) { %>
-                                <ul class="tag-list">
-                                    <% _.each( item.cpt_tag, function( tag ) { %>
-                                    <li><a href="/tag/<%= tag.slug %>"><%= tag.name %></a></li>
-                                    <% } ); %>
-                                </ul>
-                                <% } %>
-                                <a class="c-call-to-action c-glyph" href="<%= item.link %>" id="post-<?php the_ID(); ?>">
-                                    <span>Read More <i class="fa fa-angle-double-right" aria-hidden="true"></i></span>
-                                </a>-->
                             </div>
-
                         </div>
                     </div>
                 </div>
                 <% }); %>
             </div>
-            <!--    <ul class="see-more-link align-center">
-                    <li><a class="btn btn-light" role="button" title="Web Portfolio" id="scEvent" href="#web-portfolio">More Work <i class="fa fa-angle-double-right"></i></a></li>
-                </ul>-->
+                <ul class="see-more-link align-center">
+                    <li><a class="btn btn-light" role="button" title="Web Portfolio" id="scEvent" href="#recentWork">More Work <i class="fa fa-angle-double-right"></i></a></li>
+                </ul>
         </div>
     </script>
     <section id="recentWork-rec"  class="my-container"></section>
