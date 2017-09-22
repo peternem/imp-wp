786186e0fa687ad7a6c97b36674f28d9298f8aba
MJPeternell <mjpeternell@gmail.com>
2017-09-21 18:48:28 -0700

    social icons fixes 2

--- wp-content/themes/megatherium/underscore-templates-social-icons.php
+++ wp-content/themes/megatherium/underscore-templates-social-icons.php
@@ -3,7 +3,7 @@
     <% _.each(options, function(item,key,list){ %>
     <% _.each( item.acf.social_links, function(social) { %>
     <li class="social-link-item">  
-        <a href="<%= social.social_url %>" title="<%= social.social_title %>" target="_blank">
+        <a href="http://www.<%= social.social_url %>" title="<%= social.social_title %>" target="_blank">
             <i class="fa <%= social.social_icon %>" aria-hidden="true"></i>    
         </a>
     </li>
